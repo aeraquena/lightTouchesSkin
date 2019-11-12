ee8864f45e6489358f0521a9ea8aa68eff733a39
Claire Kwong <clairebkwong@gmail.com>
2019-11-12 15:47:42 +0000

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include "ofApp.h"
+
+int main() {
+    // Fullscreen one window
+    ofSetupOpenGL(1920,1080,OF_FULLSCREEN);
+    ofRunApp(new ofApp());
+    
+    // Fullscreen spans multiple windows
+    /*ofGLFWWindowSettings settings;
+    settings.multiMonitorFullScreen = true;
+    settings.windowMode = OF_FULLSCREEN;
+    ofCreateWindow(settings);
+    ofRunApp(new ofApp);*/
+    
+    // Windowed
+    //ofSetupOpenGL(2560, 1024, OF_WINDOW);
+    //ofRunApp(new ofApp());
+}
